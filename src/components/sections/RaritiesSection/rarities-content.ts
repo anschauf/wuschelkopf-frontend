e59e728ec59ac0e9326b2bad078143e3f8aba46c
Andy <andreas.schaufelbuehl@gmail.com>
2022-07-02 23:07:06 +0200

    Adapt rarities

--- src/components/sections/RaritiesSection/rarities-content.ts
+++ src/components/sections/RaritiesSection/rarities-content.ts
@@ -31,31 +31,31 @@ import IMG_HAIREXTENSION_BOTH from '../../../images/card-slider/hairextension_bo
 export const skinRarities: ICardUnit[] = [
     {
         'image': IMG_SKIN_DARK,
-        'label': '46% Drop chance',
+        'label': '36% Drop chance',
         'title': 'DARK',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'Together with the light skin, this is the most common tier.',
         'version': "purple"
     },
     {
         'image': IMG_SKIN_LIGHT,
-        'label': '46% Drop chance',
+        'label': '36% Drop chance',
         'title': 'LIGHT',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'Also the most common tier, but they get faster sun-burned.',
         'version': "purple"
     },
     {
         'image': IMG_SKIN_GHOST,
-        'label': '6% Drop chance',
+        'label': '16% Drop chance',
         'title': 'GHOST',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'A rarer skin, as this is a dead Wuschelkopf returning as a transparent ghost back to the world.',
         'version': "purple"
 
     },
     {
         'image': IMG_SKIN_SKELETT,
-        'label': '2% Drop chance',
+        'label': '8% Drop chance',
         'title': 'SKELETT',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'The rarest are undead bodies. The skeletal Wuschelkopf come with their own facial models making them the most wanted.',
         'version': "purple"
     }
 ]
@@ -63,31 +63,31 @@ export const skinRarities: ICardUnit[] = [
 export const hairRarities: ICardUnit[] = [
     {
         'image': IMG_HAIR_NORMAL,
-        'label': '25% Drop chance',
-        'title': 'NORMAL',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
-        'version': "orange"
-    },
-    {
-        'image': IMG_HAIR_RAINBOW,
-        'label': '25% Drop chance',
-        'title': 'RAINBOW',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'label': '80% Drop chance',
+        'title': 'Normal',
+        'description': 'Unicolor, but with 10 different appealing colors. All of them have the same drop chance.',
         'version': "orange"
     },
     {
         'image': IMG_HAIR_SHINING,
-        'label': '25% Drop chance',
+        'label': '12% Drop chance',
         'title': 'SHINING',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'Shining hair coming in three different colors, which let your Wuschelkopf shine out of the mass.',
         'version': "orange"
 
     },
     {
+        'image': IMG_HAIR_RAINBOW,
+        'label': '5% Drop chance',
+        'title': 'RAINBOW',
+        'description': 'Multicolored hairs. Wear it with pride.',
+        'version': "orange"
+    },
+    {
         'image': IMG_HAIR_BURNING,
-        'label': '25% Drop chance',
+        'label': '3% Drop chance',
         'title': 'BURNING',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'Just bad-ass!',
         'version': "orange"
     }
 ]
@@ -95,31 +95,31 @@ export const hairRarities: ICardUnit[] = [
 export const handItemRarities: ICardUnit[] = [
     {
         'image': IMG_HANDITEM_NONE,
-        'label': '25% Drop chance',
+        'label': '4% Drop chance',
         'title': 'NONE',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'No items at all. But who needs a item, when he/she just looks awesome.',
         'version': "green"
     },
     {
         'image': IMG_HANDITEM_LEFTONLY,
-        'label': '25% Drop chance',
+        'label': '4% Drop chance',
         'title': 'Left',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'Only for rare lefties.',
         'version': "green"
     },
     {
         'image': IMG_HANDITEM_RIGHTONLY,
-        'label': '25% Drop chance',
+        'label': '46% Drop chance',
         'title': 'Right',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'Many cool items are found here=).',
         'version': "green"
 
     },
     {
         'image': IMG_HANDITEM_BOTH,
-        'label': '25% Drop chance',
+        'label': '46% Drop chance',
         'title': 'Both',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'Because holding a teddy and a lightsaber simultaneously is just cool.',
         'version': "green"
     }
 ]
@@ -159,31 +159,31 @@ export const hairdecoRarities: ICardUnit[] = [
 export const hairExtensionsRarities: ICardUnit[] = [
     {
         'image': IMG_HAIREXTENSION_NONE,
-        'label': '25% Drop chance',
+        'label': '35% (m), 50% (f) Drop chance',
         'title': 'None',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'Basic Wuschelkopf hair-cut. Simply stylish',
         'version': "purple"
     },
     {
         'image': IMG_HAIREXTENSION_HAIRCUT,
-        'label': '25% Drop chance',
+        'label': '35% (m), 50% (f) Drop chance',
         'title': 'Haircut',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'Beautiful hair-bread for great looks.',
         'version': "purple"
     },
     {
         'image': IMG_HAIREXTENSION_BEARD,
-        'label': '25% Drop chance',
+        'label': '15% (m) Drop chance',
         'title': 'Beard',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'For all the hipster around us.',
         'version': "purple"
 
     },
     {
         'image': IMG_HAIREXTENSION_BOTH,
-        'label': '25% Drop chance',
+        'label': '15% (m) Drop chance',
         'title': 'Both',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.',
+        'description': 'Wow! So much Wuschel',
         'version': "purple"
     }
 ]
